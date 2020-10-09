42750ca22317ff9e109d076b8b92ecac0ed5762a
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-09 11:19:01 -0500

    Refactor my_select  rspec test suit

--- spec/my_select_spec.rb
+++ spec/my_select_spec.rb
@@ -5,21 +5,22 @@ require './bin/script.rb'
 describe Enumerable do
   let(:arr) { [1, 2, 3] }
   let(:range) { (1..3) }
+  let(:block) { proc { |x| x > 1 } }
 
   describe '.my_select' do
     context '#my_select behaviour' do
       example 'behaves has .select, takes an array' do
-        expect(arr.my_select { |x| x > 1 }).to eq(arr.select { |x| x > 1 })
+        expect(arr.my_select(&block)).to eq(arr.select(&block))
       end
 
       example 'behaves has .select, takes a range' do
-        expect(range.my_select { |x| x > 1 }).to eq(range.select { |x| x > 1 })
+        expect(range.my_select(&block)).to eq(range.select(&block))
       end
     end
 
     context '.my_select returning values' do
       example 'Returns and array when a block is passed' do
-        expect(range.my_select { |x| x > 1 }).to eq([2, 3])
+        expect(range.my_select(&block)).to eq([2, 3])
       end
       example 'Returns an enumerator when no block is passed' do
         expect(range.my_select).to be_an Enumerator

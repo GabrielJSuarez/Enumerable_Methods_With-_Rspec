a262da5f2f9a986ee088e29df39a229ff1377aa9
Gabriel Suárez <ginnlaiho@gmail.com>
2020-10-09 12:30:36 -0500

    Change script file name

--- spec/my_all?_spec.rb
+++ spec/my_all?_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './bin/enumerable'
+require './bin/main'
 
 # rubocop:disable Metrics/BlockLength
 
--- spec/my_map_spec.rb
+++ spec/my_map_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './bin/enumerable'
+require './bin/main'
 
 describe Enumerable do
   let(:arr) { [1, 2, 3] }
